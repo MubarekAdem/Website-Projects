75f51d62f74517637d2399449361624c9a7f6089
MubarekAdem <156066237+MubarekAdem@users.noreply.github.com>
2024-07-29 08:30:02 -0700

    Delete Music Player directory

--- Music Player/app/playmusic/components/progress.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-let Progress = React.createClass({
-  render() {
-    return <p>progress：{this.props.progress}s</p>;
-  },
-});
-
-export default Progress;
--- Music Player/readme.md
+++
@@ -1,1 +0,0 @@
-This is A music player That is build using ReactJS
